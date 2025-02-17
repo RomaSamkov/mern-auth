d836c07f5a99b97ecff7129ac352895c59539de3
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-17 16:58:41 +0200

    add form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLoginUser = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onLoginUser}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
