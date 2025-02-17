e6f374965c03794417e43a24aa83907d70dce2c8
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-17 16:58:48 +0200

    add form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onRegisterUser = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onRegisterUser}>
+        <label htmlFor="username">Name:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={data.password}
+          onChange={(e) => setData({ ...data, password: e.target.value })}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
