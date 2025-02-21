a033052b132aa3fd436ccf567f6fdc7992accc7c
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-21 17:23:20 +0200

    add baseURL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
