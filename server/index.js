eeb2fd56d9d93080187d05eb3a509b70e6bc8b6d
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-21 17:23:37 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import authRotes from "./routes/authRoutes.js";
+import cors from "cors";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 const app = express();
+
+// app.use(express.json());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use("/", authRotes);
+app.listen(port, () => console.log(`Server is running on port ${port}`));
