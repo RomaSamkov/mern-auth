6b4933dea58e5930f501645af8384bf26c12b979
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-24 16:58:21 +0200

    add register

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.json("Hello server app.");
 });
+
+router.post("/register", registerUser);
 
 export default router;
