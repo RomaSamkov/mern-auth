7278ffcfe1ed2c04d40394ad2a9670825d2b1069
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-24 16:57:43 +0200

    add mongoose

--- server/index.js
+++ server/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import authRotes from "./routes/authRoutes.js";
 import cors from "cors";
+import mongoose from "mongoose";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database is connected."))
+  .catch((error) => console.log("Database is not connected.", error));
+
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 app.use(
   cors({
