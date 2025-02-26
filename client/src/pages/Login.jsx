b205bdc8e3ca84999eaa47c8afea9bc0ebb06f1c
RomaSamkov <Romansamkov.rs@gmail.com>
2025-02-26 13:27:34 +0200

    add login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [data, setData] = useState({
     email: "",
     password: "",
@@ -9,18 +12,34 @@ const Login = () => {
 
   const onLoginUser = async (e) => {
     e.preventDefault();
-    const response = await axios.get("/");
-    console.log("🚀 ~ onLoginUser ~ response:", response.data);
+    const { email, password } = data;
+    try {
+      const { data } = await axios.post("/login", {
+        email,
+        password,
+      });
+
+      if (data.error) {
+        toast.error(data.error);
+      } else {
+        setData({});
+        toast.success("Login is successful.");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
+      <h2>LOGIN</h2>
       <form onSubmit={onLoginUser}>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           name="email"
           id="email"
-          value={data.email}
+          value={data.email || ""}
           onChange={(e) => setData({ ...data, email: e.target.value })}
         />
         <label htmlFor="password">Password:</label>
@@ -28,7 +47,7 @@ const Login = () => {
           type="password"
           name="password"
           id="password"
-          value={data.password}
+          value={data.password || ""}
           onChange={(e) => setData({ ...data, password: e.target.value })}
         />
         <button type="submit">Login</button>
